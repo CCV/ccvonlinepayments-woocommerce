fe9832a73133179d928752c1450fd9a08956655d
PeterCMDC <peter@cmdc.eu>
2021-01-06 12:39:32 +0100

    Fix images

--- copyImages.sh
+++ copyImages.sh
@@ -5,8 +5,8 @@ rm -Rf "$(pwd)"/src/images
 mkdir "$(pwd)"/src/images
 mkdir "$(pwd)"/src/images/methods
 
-cp -Rf "$(pwd)"/vendor/ccvonlinepayments/images/*.png "$(pwd)"/src/images
-cp -Rf "$(pwd)"/vendor/ccvonlinepayments/images/methods/*.png "$(pwd)"/src/images/methods
+cp -Rf "$(pwd)"/vendor/ccv/images/*.png "$(pwd)"/src/images
+cp -Rf "$(pwd)"/vendor/ccv/images/methods/*.png "$(pwd)"/src/images/methods
 cd "$(pwd)"/src/images/methods;
 mogrify -resize 999x32 *.png
 cd ../../../
