36d3db665989f38921f42066ac6723713ea7e518
PeterCMDC <peter@cmdc.eu>
2021-03-11 15:54:18 +0100

    Update metadata and bump version

--- src/ccvonlinepayments.php
+++ src/ccvonlinepayments.php
@@ -1,11 +1,11 @@
 <?php
 /*
  * Plugin Name: CCV Online Payments for Woocommerce
- * Plugin URI: https://github.com/CCV-Group-B-V/ccvonlinepayments-woocommerce
- * Description: WooCommerce plugin for CCV Online Payments
+ * Plugin URI: https://github.com/CCV/ccvonlinepayments-woocommerce
+ * Description: Official CCV Payment Services plugin for WooCommerce
  * Author: CCV Online Payments
  * Author URI: https://www.ccv.eu/nl/betaaloplossingen/betaaloplossingen-online/ccv-online-payments/
- * Version: 1.2.0
+ * Version: 1.2.1
  * Requires at least: 5.4
  * Tested up to: 5.6
  * WC requires at least: 4.2
